60df40d4f29e0d6b78143f268831aa4649ad4adb
HubertDudaDeveloper <102989117+HubertDudaDeveloper@users.noreply.github.com>
2022-11-26 15:37:59 +0100

    Navigation :art:

--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <button :style="customStyle" :class="customClass" class="btn" v-if="true"><span v-if="props.customClass.includes('bi')" class="d-none d-sm-inline">{{label}}</span><span v-else class="d-inline">{{label}}</span></button>
+    <button :style="customStyle" :class="customClass" class="btn" v-if="true"><span v-if="props.customClass?.includes('bi')" class="d-none d-lg-inline">{{label}}</span><span v-else class="d-inline">{{label}}</span></button>
 </template>
 <script setup lang="ts">
 import { defineProps } from 'vue'
--- src/components/MainNavComponent.vue
+++ src/components/MainNavComponent.vue
@@ -1,27 +1,56 @@
 <template>
-    <nav class="main__nav col-2 vh-100 d-md-flex flex-column gap-5">
-        <img src="@/assets/logo_low.png" class="col-12"/>
-        <strong>
-            <ul class="main__nav-list col-12 d-flex flex-column gap-5">
+    <nav id="main__nav" class="main__nav col-12 col-sm-1 col-lg-2 vh-25 d-flex justify-content-between justify-content-sm-start flex-row flex-sm-column gap-5">
+        <img src="@/assets/logo_low.png" class=" col-sm-12 mt-2 mt-md-0"/>
+            <ButtonComponent
+                class="bi bi-list rounded-0 h-50 m-3 glass text-white d-sm-none d-flex align-self-center align-items-center rounded-2"
+                :disabled="false"
+            />
+            <ul class="main__nav-list col-12 d-none d-sm-flex flex-sm-column gap-5 pt-5 pt-md-1">
                 <ButtonComponent
                     v-for="(item, index) in props.navComponent"
                     :key="index"
                     class="text-white"
-                    :customStyle="item.style"
                     :customClass="item.class"
                     :label="item.label"
                     :disabled="item.disabled"
                 />
+                <ButtonComponent
+                    class="text-white col-3 align-self-center"
+                    :customClass="'glass p-2 bi bi-arrow-bar-left rounded-1'"
+                    :label="''"
+                    :disabled="false"
+                    @click="resizeBar($event)"
+                    v-show="screen"
+                />
             </ul>
-        </strong>
     </nav>
 </template>
 <script setup lang="ts">
 import ButtonComponent from './ButtonComponent.vue'
-import { defineProps } from 'vue'
+import { defineProps, ref, computed } from 'vue'
 
 const props = defineProps({
   navComponent: Array
 })
+
+const screen = computed(() => {
+  return window.screen.width >= 1024
+})
+
+const resizeBar = (event) => {
+  const nav = document.getElementById('main__nav')
+  const before = document.querySelectorAll('span.d-none,d-lg-inline')
+
+  if (event.target.style.rotate === '180deg') {
+    event.target.style.rotate = '0deg'
+  } else {
+    event.target.style.rotate = '180deg'
+  }
+
+  nav.classList.toggle('col-md-2')
+  before.forEach((item) => {
+    item.classList.toggle('d-lg-inline')
+  })
+}
 
 </script>
